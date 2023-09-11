0e207ff3a77b4f9867a127bbe82c37ee680086a7
StirlingSmith <stirlingjamessmith@gmail.com>
2023-09-11 12:40:26 +1200

    tenas

--- src/WritePostgres.jl
+++ src/WritePostgres.jl
@@ -2,10 +2,11 @@ module WritePostgres
 
     include("process_todays_articles.jl")
     include("ReadWritePostgres.jl")
+    include("onboard_user.jl")
 
     # Main Functions 
     export process_todays_articles
-    # export onboard_user
+    export onboard_user
     export collect_data
 
     # Additional useful functions that are imported by MakeWebJSON
