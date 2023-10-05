93c67c5f291e474a3437d17de01e5fab48f78baf
Stirling Smith <stirlingjamessmith@gmail.com>
2023-10-06 07:38:46 +1300

    add plot title and axis titles

--- src/Benchmarking/benchmark_script.jl
+++ src/Benchmarking/benchmark_script.jl
@@ -2,13 +2,14 @@ date=Date("2023-10-01") # Date we are taking for our RDPG
 threshold = 3 # Number of std difference from baseline to be qualified as anomally
 noise = 200 # Number of edges set to 0 as well as number of edges set to one
 seq_len = 51 # Number of samples in baseline (can take a while to do embedding so don't make too big)
-kw = "facebook"
+kw = "musk"
 
 include("utils/get_base_rdpg.jl")
 include("utils/resample_rdpg.jl")
 include("utils/randomise_sample.jl")
 include("utils/mean_word_distance_seq.jl")
 include("utils/embedding.jl")
+include("noisy_rdpg_dist.jl")
 
 rdpg = get_base_rdpg(date, kw)
 
@@ -23,26 +24,25 @@ e = [d[2] for d in distributions]
 
 using PlotlyJS
 trace= scatter(
-       x=noise_levels, y=avg,
+       x=100 .*noise_levels, y=avg,
        mode="markers",
        error_y=attr(type="data", array=3 .*e)
       )
 
 layout = Layout(
     title=attr(
-        text= "",
+        text= "Mean Word Distances vs Network Noise ($(kw))",
         y=0.9,
         x=0.5,
         xanchor= "center",
         yanchor= "top"
-    )
+    ),
+    xaxis_title= "noise (% of network size)",
+    yaxis_title= "mean word distance"
 )
 
 PlotlyJS.plot(trace, layout)
-plot(
-    [],
-    Layout(title="my title",)
-)
+
 # baseline = resample_rdpg(rdpg,51)
 
 # get_L(E) = E[:L̂]
