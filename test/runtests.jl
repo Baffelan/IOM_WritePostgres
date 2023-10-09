ee01720eb14cf35acada1911f80b3e866b4ac8b0
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-10 10:53:46 +1300

    rough test for aligning_matrix!

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,18 @@ using Test
 
 using DataFrames
 
-embs = [[1 0; 0 1; 0 2], [0 1; 1 1; 2 1]]
+embs = [[1.0 0.0; 0.0 1.0; 1.0 2.0], [0.0 1.0; 1.0 1.0; 2.0 1.0]]
 idxs = [Dict("is"=>1, "was"=>2, "were"=>3), Dict("has"=>1, "will"=>2, "is"=>3)]
 df = DataFrame(embedding=embs, token_idx=idxs)
 
-targets = (Float32[1.0 0.0; 0.0 1.0; 1.0 2.0; 0.0 0.0; 0.0 0.0], Float32[2.0 1.0; 0.0 0.0; 0.0 0.0; 0.0 1.0; 1.0 1.0], ["is", "was", "were", "has", "will"])
+interacting_targets = (Float32[1.0 0.0; 0.0 1.0; 1.0 2.0; 0.0 0.0; 0.0 0.0], Float32[2.0 1.0; 0.0 0.0; 0.0 0.0; 0.0 1.0; 1.0 1.0], ["is", "was", "were", "has", "will"])
+
+
+W = IOM_WritePostgres.WordNetwork(zeros(Float32, (1,1)), (L̂=embs[1],R̂=embs[2]),idxs[1], nothing)
+transformer = [0.0 1.0; 1.0 0.0]
+ref = embs[1]*transformer
 
 @testset "IOM_WritePostgres.jl" begin
-    @test targets == IOM_WritePostgres.create_interacting_embedding(df[1,:], df[2,:])
-
+    @test interacting_targets == IOM_WritePostgres.create_interacting_embedding(df[1,:], df[2,:])
+    @test isapprox(transformer, IOM_WritePostgres.aligning_matrix!(W, A=ref, tokens=collect(keys(idxs[1]))), rtol=1e-5)
 end
