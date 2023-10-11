aed5392d1e1bc68d348e3d11fecbd843e0b5cb3e
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-11 15:26:33 +1300

    Added docstring

--- src/Benchmarking/utils/get_base_rdpg.jl
+++ src/Benchmarking/utils/get_base_rdpg.jl
@@ -1,7 +1,14 @@
 set_max(n, m) = n>m ? m : n
 set_min(n, m) = n<m ? m : n
 
-function get_base_rdpg(date,kw)
+"""
+Takes a date and keyword, then retrieves an embedding from `processedarticles` and calculates an RDPG.
+
+# Arguments
+- `date::Date`: date of embedding.
+- `kw::String`: keyword of embedding.
+"""
+function get_base_rdpg(date::Date,kw::String)
     df = query_postgres("processedarticles", "back", condition = replace("where date='$(date)' and keyword='$(kw)'", "\""=>""))
     A = df.embedding[1]'*df.embedding[1]
     A = set_max.(A, [1.0])
