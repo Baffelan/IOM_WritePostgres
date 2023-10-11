0116f895a39a5dbdfa9a1136d639794d6b9f4386
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-11 15:22:33 +1300

    Added docstring

--- src/Benchmarking/utils/embedding.jl
+++ src/Benchmarking/utils/embedding.jl
@@ -1,3 +1,10 @@
+""" 
+A wrapper for getting an SVD embedding of a graph using the `fast_svd` function as the method.
+    
+# Arguments
+- `graph::AbstractMatrix{AbstractFloat}`: the graph which is being decomposed.
+- `emb_dim::Int`: the dimension of the embedding.
+"""
 function embedding(graph, emb_dim)
     DotProductGraphs.svd_embedding(graph, fast_svd, emb_dim)[:L̂]
 end
