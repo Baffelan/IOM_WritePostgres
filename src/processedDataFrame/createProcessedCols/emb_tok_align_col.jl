1354f3d8087ee25ad734a1d031ca61480a08aa42
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-18 13:46:42 +1300

    Added Argument documentation

--- src/processedDataFrame/createProcessedCols/emb_tok_align_col.jl
+++ src/processedDataFrame/createProcessedCols/emb_tok_align_col.jl
@@ -1,3 +1,14 @@
+"""
+Finds the embedding, token to index dictionary, and aligning matrix for each date in `dates` from the `raw_df`.
+
+# Arguments
+- `raw_df::DataFrame`: a data frame from raw table (in practice this will be a filtered data frame with all articles from a given keyword).
+- `Dates::Vector{Union{Missing, Date}}`: The dates for which we want the embedding, token to index dictionary, and aligning matrix.
+- `alignment_tokens::Base.AbstractVecOrTuple`: a tuple of common words for trying to align the embeddings of each word network.
+- `refmatrix::Union{Nothing, AbstractArray}`: an m✖n matrix that each embedding is being aligned to, the rows correspond to the words in alignment_tokens.
+- `emb_dim::Int`: The dimension of the embedding of the work networks (if refmatrix is not nothing, emb_dim must equal n).
+- `filtered_word_counts::Vector{Dict{T, Int}} where{T<:AbstractString}`: A list of dictionaries that indicate which words are occur frequently enough to be used in the word network.    
+"""
 function emb_tok_align_col(raw_df::DataFrame, 
                            dates::Vector{Union{Missing, Date}}, 
                            alignment_tokens::Base.AbstractVecOrTuple,
