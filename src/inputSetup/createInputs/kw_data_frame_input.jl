8739c0d60f099e10c314c9fc7ec775ff2092dc2e
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-19 13:12:02 +1300

    update for integration

--- src/inputSetup/createInputs/kw_data_frame_input.jl
+++ src/inputSetup/createInputs/kw_data_frame_input.jl
@@ -5,7 +5,7 @@ This function takes a keyword and a dataframe and returns the rows that have the
 - `kw::String`: The keyword being searched for.
 - `df::DataFrame`: A data frame with a `:body` column containing Strings 
 """
-function kw_data_frame_input(kw::String, df::DataFrame)
+function kw_data_frame_input(kw::AbstractString, df::DataFrame)
     has_kw = occursin.([replace(kw, "\""=>"")], format_text.(df.body))
     kw_df = df[has_kw,:]
     kw_df#[1:min(50,nrow(kw_df)),:]
